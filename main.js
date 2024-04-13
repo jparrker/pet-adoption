a3275e4bda9358ecd4e7ee3633605427f2dc2506
jparrker <jasonkk.parker@gmail.com>
2024-04-13 18:56:58 -0400

    finished pet filtering

--- main.js
+++ main.js
@@ -24,15 +24,45 @@ async function petsArea() {
     const clone = template.content.cloneNode(true)
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-card").dataset.species = pet.species
     clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
     if (!pet.photo) pet.photo = "./images/fallback.jpg"
     clone.querySelector('.pet-card-photo img').src = pet.photo
     wrapper.appendChild(clone)
-
   });
   document.querySelector(".list-of-pets").appendChild(wrapper) //only update the DOM once. 
-
 }
+
+const allButtons = document.querySelectorAll(".pet-filter button")
+allButtons.forEach((element) => {
+  element.addEventListener("click", handleButtonClick)
+})
+
+function handleButtonClick(e) {
+
+  // remove active class from all buttons
+  allButtons.forEach(el => el.classList.remove("active"))
+
+  // add active class to clicked button
+  e.target.classList.add("active")
+  // filter pets
+  const currentFilter = e.target.dataset.filter
+
+  document.querySelectorAll(".pet-card").forEach((el) => {
+    if (currentFilter == el.dataset.species || currentFilter == 'all') {
+      el.style.display = 'grid'
+    } else {
+      el.style.display = 'none'
+    }
+  })
+}
+
+// pet filter
+
+
+
+
+
 
 start()
 petsArea()
